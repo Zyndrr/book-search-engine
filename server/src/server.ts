02b112559d841162e0c53a94a3804a75d408bb0d
Zyndrr <erin.d.mckanna@gmail.com>
2024-12-07 13:53:15 -0700

    Update server

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,12 @@ import { ApolloServer } from "@apollo/server"; // Note: Import from @apollo/serv
 import { expressMiddleware } from "@apollo/server/express4";
 import { typeDefs, resolvers } from "./schemas/index.js";
 import { authenticateToken } from "./utils/auth.js";
-import cors from "cors";
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const server = new ApolloServer({
   typeDefs,
@@ -19,7 +24,6 @@ const startApolloServer = async () => {
 
   const PORT = process.env.PORT || 3001;
   const app = express();
-  app.use(cors());
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
@@ -32,10 +36,10 @@ const startApolloServer = async () => {
   );
 
   if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../client/dist")));
+    app.use(express.static(path.join(__dirname, "../../client/dist")));
 
     app.get("*", (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+      res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
     });
   }
 
